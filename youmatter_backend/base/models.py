1bc1b73a77d7ebfbf8eb33f99a6577555dbba6c4
Elisha Norman <75836861+TRUDER735@users.noreply.github.com>
2024-04-14 19:59:44 +0200

    #added users tables

--- youmatter_backend/base/models.py
+++ youmatter_backend/base/models.py
@@ -15,23 +15,36 @@ class CustomUser(AbstractUser):
 
 class Admin(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    admin_name = models.CharField(max_length=100)
+    admin_surnme = models.CharField(max_length=100)
+    admin_email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    # admin_emailpassword = models.CharField(max_length=100)
     admin_code = models.CharField(max_length=10)
+
 
     def __str__(self):
         return self.user.username
 
 class Agent(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    agent_name = models.CharField(max_length=100)
+    agent_surnme = models.CharField(max_length=100)
+    agent_email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    # agent_emailpassword = models.CharField(max_length=100)
     agent_code = models.CharField(max_length=10)
-    location = models.CharField(max_length=100)
+    agent_location = models.CharField(max_length=100)
 
     def __str__(self):
         return self.user.username
 
 class Client(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-    address = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
+    client_name = models.CharField(max_length=100)
+    client_surnme = models.CharField(max_length=100)
+    client_address = models.CharField(max_length=100)
+    client_phonenumber = models.CharField(max_length=15)
 
     def __str__(self):
         return self.user.username
